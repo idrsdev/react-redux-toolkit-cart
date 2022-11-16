68e1405040d34ffdd7b34aa1dd51ca7487c8023e
Idris <115788425+idrsdev@users.noreply.github.com>
2022-11-16 16:38:10 +0500

    A shopping cart

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import CartContainer from "./components/CartContainer";
+import Navbar from "./components/Navbar";
+
+import { useDispatch, useSelector } from "react-redux";
+import { calculateTotals, getCartItems } from "./features/cart/cartSlice";
+
+import { useEffect } from "react";
+import Modal from "./components/Modal";
+
+function App() {
+  const { cartItems, isLoading } = useSelector((store) => store.cart);
+  const dispatch = useDispatch();
+
+  const { isOpen } = useSelector((state) => state.modal);
+
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems]);
+
+  useEffect(() => {
+    dispatch(getCartItems("storeName"));
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className="loading">
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
+
+  return (
+    <main>
+      {isOpen && <Modal />}
+      <Navbar />
+      <CartContainer />
+    </main>
+  );
+}
+export default App;
